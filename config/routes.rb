c72816c00e69439a6698f8126f9154057ae7dbba
david <dmatthe@yahoo.com>
2022-06-14 16:54:14 -0400

    Finish home page and add an about an login dummy page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  get 'pages_home',to: 'pages#home'
+  get 'about',to: 'pages#about'
+  get 'login',to: 'pages#login'
+  get 'logout',to: 'pages#logout'
 end
